bc4eccd06f69f678ccc5be9a624a7a2fd1ef5c46
nikhilmmahajan <nikhil.mm@gmail.com>
2018-02-19 18:21:57 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/choose_k_out_of_N_options.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/choose_k_out_of_N_options.dir/main.cpp.obj"
+  "choose_k_out_of_N_options.pdb"
+  "choose_k_out_of_N_options.exe"
+  "choose_k_out_of_N_options.exe.manifest"
+  "libchoose_k_out_of_N_options.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/choose_k_out_of_N_options.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int choose(int n, int k)
+{
+    if (k==n || k ==0)
+        return 1;
+
+    return choose(n-1,k-1) + choose(n-1,k);
+
+}
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int counter =1;
+    int n, k ;
+    while(counter)
+    {
+        cout<< "Enter whats k number of elements you want to choose from n total elements :";
+        cin>>k ;
+        cin>>n;
+
+        cout<<"\nTotal number of ways to choose "<<k <<" elements out of "<< n <<" is :"<<choose(n,k);
+
+        cout<< "\n\nEnter 1 to try again or 0 to EXIT";
+        cin>>counter;
+    }
+    return 0;
+}
